6ccff674e0385f1698bae5dd5342a9026c08bf83
CodeMaker4 <codemaker04@gmail.com>
2022-09-26 13:43:04 +0200

    added player movement

--- entities/playerEntity.ts
+++ entities/playerEntity.ts
@@ -7,4 +7,14 @@ export class PlayerEntity extends DungineEntity {
     constructor(dungine: Dungine, room: DungineRoom) {
         super(dungine, room, Vec2d.randBox(room.size.x, room.size.y), Vec2d.rand2D(500), 20);
     }
+
+    tick(dt) {
+        this.vel.mult(0.9);
+        const MOVE_SPEED = 50;
+        if (this.dungine.controls.heldCodes.has("KeyA")) this.vel.addXY(-MOVE_SPEED,0);
+        if (this.dungine.controls.heldCodes.has("KeyD")) this.vel.addXY(MOVE_SPEED,0);
+        if (this.dungine.controls.heldCodes.has("KeyW")) this.vel.addXY(0,-MOVE_SPEED);
+        if (this.dungine.controls.heldCodes.has("KeyS")) this.vel.addXY(0,MOVE_SPEED);
+        super.tick(dt);
+    }
 }
