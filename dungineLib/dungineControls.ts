5d113cfd2cc47306aafcf493b6cfdfe4a7dba0e7
CodeMaker4 <codemaker04@gmail.com>
2022-09-26 13:42:10 +0200

    added keycodes next to keys

--- dungineLib/dungineControls.ts
+++ dungineLib/dungineControls.ts
@@ -1,30 +1,34 @@
 import { DungineFunctionList } from "./dungineFunctionList.js";
 
 export class DungineControls {
-    onkeydown: DungineFunctionList<(key: string, heldKeys: Set<string>) => void>
-    onkeyup: DungineFunctionList<(key: string, heldKeys: Set<string>) => void>
-    onkeyheldtick: DungineFunctionList<(heldKeys: Set<string>, dt: number) => void>
+    onkeydown: DungineFunctionList<(e: KeyboardEvent, controls:DungineControls) => void>
+    onkeyup: DungineFunctionList<(e: KeyboardEvent, controls:DungineControls) => void>
+    onkeyheldtick: DungineFunctionList<(dt: number, controls:DungineControls) => void>
     heldKeys: Set<string>
+    heldCodes: Set<string>
 
     constructor() {
         this.onkeydown = new DungineFunctionList();
         this.onkeyup = new DungineFunctionList();
         this.onkeyheldtick = new DungineFunctionList();
         this.heldKeys = new Set();
+        this.heldCodes = new Set();
 
         window.addEventListener("keydown", (e) => {
             if (e.repeat) return;
-            this.heldKeys.add(e.key);
-            this.onkeydown.excecute([e.key, this.heldKeys]);
+            this.heldKeys.add(e.key.toLowerCase());
+            this.heldCodes.add(e.code)
+            this.onkeydown.excecute([e, this]);
         });
 
         window.addEventListener("keyup", (e) => {
-            this.onkeydown.excecute([e.key, this.heldKeys]);
-            this.heldKeys.delete(e.key);
+            this.onkeyup.excecute([e, this]);
+            this.heldKeys.delete(e.key.toLowerCase());
+            this.heldCodes.delete(e.code);
         });
     }
 
     tick(dt) {
-        this.onkeyheldtick.excecute([this.heldKeys, dt]);
+        this.onkeyheldtick.excecute([dt, this]);
     }
 }
